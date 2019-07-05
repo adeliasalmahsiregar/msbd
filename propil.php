7fd5845fa4ea9530a4df6a4603b1e49c4662f189
nurainii <39108873+nurainii@users.noreply.github.com>
2019-07-05 08:31:59 +0700

    Add files via upload

---
+++ propil.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	 <meta charset="utf-8">
+  <title>Donasi Beasiswa</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="" name="keywords">
+  <meta content="" name="description">
+  
+   <!-- Favicons -->
+  <link href="img/favicon.png" rel="icon">
+  <link href="img/apple-touch-icon.png" rel="apple-touch-icon">
+  
+	<!-- Bootstrap -->
+	<link href="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.no-icons.min.css" rel="stylesheet">
+	<!-- Icon font -->
+	<link href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+	<!-- Fonts -->
+	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Alice|Open+Sans:400,300,700">
+	<!-- Custom styles -->
+	<link rel="stylesheet" href="assets/css/styles.css">
+
+	<!--[if lt IE 9]> <script src="assets/js/html5shiv.js"></script> <![endif]-->
+</head>
+<body>
+
+<header id="header">
+	<div id="head" class="parallax" parallax-speed="1">
+		<h1 id="logo" class="text-center">
+			<img class="img-circle" src="assets/images/1.png" alt="">
+			<span class="title">Saknah Zahirah</span>
+			<span class="tagline">Kelas IV Sd Juara</span>
+		</h1>
+	</div>
+
+	<nav class="navbar navbar-default navbar-sticky">
+		<div class="container-fluid">
+			
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+			</div>
+
+<main id="main">
+
+	<div class="container">
+		<div class="row topspace">
+			<div class="col-sm-8 col-sm-offset-2">
+															
+				<article class="post">
+					<header class="entry-header">
+						<div class="entry-meta"> 
+							<span class="posted-on"><time class="entry-date published" date="2013-09-27">Juni 30, 2019</time></span>			
+						</div>
+						<h1 class="entry-title"><a href="single.html" rel="bookmark">Assalammualaikum Sahabat</a></h1>
+					</header>
+					<div class="entry-content">
+						<p>Saknah Adalah siswi dari Sd Juara yang menempati kelas IV, dia terlahir dari anak dari bapak fulan dan ibu fulannah</p>
+					</div>
+				</article>
+ 				
+ 
+<!-- JavaScript libs are placed at the end of the document so the pages load faster -->
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+<script src="assets/js/template.js"></script>
+
+</body>
+</html>
+<footer>
+<center>
+<div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong>Sistem Pengelolaan Donasi Beasiswa</strong>
+      </div>
+      <div class="credits">
+</center>      
+      </div>
+    </div>
+  </footer>
